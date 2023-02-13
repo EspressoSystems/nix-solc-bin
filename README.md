af5a7597f1ff5114eff74bebc8d950fa859dd256
sveitser <sveitser@gmail.com>
2023-02-13 13:10:35 +0100

    Fix `nix flake show`
    - Move overlays and templates out of `flake-utils.lib.eachSystem`.
    - Remove `aarch64-linux` from supported systems due to never having
      tested it.

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 For convenient access to all released solc version with nix.
 
-- Binaries from https://github.com/ethereum/solc-bin.
-- [Usage example](./templates/default/flake.nix).
-- Run `./update-sources.sh` to fetch new solidity builds.
+Binaries from https://github.com/ethereum/solc-bin.
 
+### Usage
+- See [the template](./templates/default/flake.nix) for example usage in a `flake.nix`.
+- Run as flake app: `nix run github:EspressoSystems/nix-solc-bin`
+- See available versions: `nix flake show github:EspressoSystems/nix-solc-bin`
+
+### Updating
+Run `./update-sources.sh` to fetch new solidity builds.
--- flake.nix
+++ flake.nix
@@ -5,23 +5,27 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system}; in
-      rec {
-        packages = import ./default.nix { inherit (pkgs) stdenv lib fetchurl autoPatchelfHook; };
-        apps = rec {
-          default = apps.solc-bin;
-          solc-bin = flake-utils.lib.mkApp { name = "solc"; drv = packages.default; };
-        };
-        overlays.default = final: prev: {
-          solc-bin = self.packages.${prev.system};
-        };
-        templates = {
-          default = {
-            path = ./templates/default;
-            description = "Solc-bin template";
+    flake-utils.lib.eachSystem
+      [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ]
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in
+        rec {
+          packages = import ./default.nix { inherit (pkgs) stdenv lib fetchurl autoPatchelfHook; };
+          apps = rec {
+            default = apps.solc-bin;
+            solc-bin = flake-utils.lib.mkApp { name = "solc"; drv = packages.default; };
           };
+        })
+    // {
+      overlays.default = final: prev: {
+        solc-bin = self.packages.${prev.system};
+      };
+      templates = {
+        default = {
+          path = ./templates/default;
+          description = "Solc-bin template";
         };
-      }
-    );
+      };
+    };
 }
